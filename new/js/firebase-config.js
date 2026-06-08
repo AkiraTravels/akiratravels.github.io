fe018231109cfea8ecd90d543ff7663106b2f811
Akira MIZUI <64358958+AkiraTravels@users.noreply.github.com>
2026-06-08 19:15:40 +0900

    Add files via upload

--- new/js/firebase-config.js
+++ new/js/firebase-config.js
@@ -13,12 +13,12 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-aut
 //    から取得した値を貼り付けてください
 // -------------------------------------------------------
 const firebaseConfig = {
-  apiKey:            "YOUR_API_KEY",
-  authDomain:        "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId:         "YOUR_PROJECT_ID",
-  storageBucket:     "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId:             "YOUR_APP_ID"
+  apiKey:            "AIzaSyCZpY0Wa99YfM1BZlZvlBa5FpQ2FuaRBp0",
+  authDomain:        "akira-travels.firebaseapp.com",
+  projectId:         "akira-travels",
+  storageBucket:     "akira-travels.firebasestorage.app",
+  messagingSenderId: "343465435050",
+  appId:             "1:343465435050:web:b34150eded57a90a8a15d3"
 };
 
 // -------------------------------------------------------
@@ -26,8 +26,8 @@ const firebaseConfig = {
 //    Cloudinary Console > Dashboard から取得
 // -------------------------------------------------------
 export const CLOUDINARY_CONFIG = {
-  cloudName:    "YOUR_CLOUD_NAME",   // 例: "my-travel-blog"
-  uploadPreset: "YOUR_UPLOAD_PRESET" // Unsigned upload preset名
+  cloudName:    "dc1l1pjid",   // 例: "my-travel-blog"
+  uploadPreset: "AKIRA travels" // Unsigned upload preset名
   // ※ Settings > Upload > Upload presets で
   //   "Unsigned" のプリセットを作成してください
 };
