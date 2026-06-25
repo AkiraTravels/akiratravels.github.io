09186043400e224ffffe5850406b2dcb745a26d6
Akira MIZUI <64358958+AkiraTravels@users.noreply.github.com>
2026-06-26 01:05:02 +0900

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,7 @@
+// Cloudflare Workers の公開エンドポイント
+export const API_BASE = "https://akira-travels-api.akiratravels.workers.dev";
+
+export const CLOUDINARY_CONFIG = {
+  cloudName:    "dc1l1pjid",
+  uploadPreset: "AKIRA travels"
+};
