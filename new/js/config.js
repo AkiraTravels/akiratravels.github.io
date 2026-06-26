b458e8ec34e547aad283bd65e133a0e1958af316
Akira MIZUI <64358958+AkiraTravels@users.noreply.github.com>
2026-06-26 23:45:49 +0900

    Delete new directory

--- new/js/config.js
+++
@@ -1,7 +0,0 @@
-// Cloudflare Workers の公開エンドポイント
-export const API_BASE = "https://akira-travels-api.akiratravels.workers.dev";
-
-export const CLOUDINARY_CONFIG = {
-  cloudName:    "dc1l1pjid",
-  uploadPreset: "AKIRA travels"
-};
